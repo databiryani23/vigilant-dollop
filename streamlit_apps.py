cb1b0200f3d489a8e370f0fc1add57f8d150cdfe
databiryani23 <134163530+databiryani23@users.noreply.github.com>
2023-06-11 23:29:00 -0400

    Update streamlit_apps.py

--- streamlit_apps.py
+++ streamlit_apps.py
@@ -1,19 +1,25 @@
-
 import streamlit as st
 from langchain.llms import OpenAI
+from langchain import PromptTemplate
 
-st.title('🦜🔗 Quickstart App')
+st.set_page_config(page_title ="🦜🔗 Blog Outline Generator App")
+st.title('🦜🔗 Blog Outline Generator App')
+openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
 
-openai_api_key = st.sidebar.text_input('OpenAI API Key')
+def generate_response(topic):
+  llm = OpenAI(model_name='text-davinci-003', openai_api_key=openai_api_key)
+  # Prompt
+  template = 'As an experienced data scientist and technical writer, generate an outline for a blog about {topic}.'
+  prompt = PromptTemplate(input_variables=['topic'], template=template)
+  prompt_query = prompt.format(topic=topic)
+  # Run LLM model and print out response
+  response = llm(prompt_query)
+  return st.info(response)
 
-def generate_response(input_text):
-  llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
-  st.info(llm(input_text))
-
-with st.form('my_form'):
-  text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
+with st.form('myform'):
+  topic_text = st.text_input('Enter keyword:', '')
   submitted = st.form_submit_button('Submit')
   if not openai_api_key.startswith('sk-'):
     st.warning('Please enter your OpenAI API key!', icon='⚠')
   if submitted and openai_api_key.startswith('sk-'):
-    generate_response(text)
+    generate_response(topic_text)
